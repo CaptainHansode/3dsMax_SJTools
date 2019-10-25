730c7d859b8431d145842321fb7ebe56ad10adf8
CaptainHansode <sakaiden@live.jp>
2019-10-25 20:45:54 +0900

    SJSetwindowTopMostを新規追加

---
+++ win/SJSetWindowTopMost/SJSetWindowTopMost/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SJSetWindowTopMost
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.runButton = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.titleEditBox = new System.Windows.Forms.TextBox();
+            this.renameButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.winListView = new System.Windows.Forms.ListView();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // runButton
+            // 
+            this.runButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.runButton.Location = new System.Drawing.Point(8, 394);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(155, 33);
+            this.runButton.TabIndex = 0;
+            this.runButton.Text = "button1";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // titleEditBox
+            // 
+            this.titleEditBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.titleEditBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.titleEditBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.titleEditBox.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.titleEditBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(229)))), ((int)(((byte)(229)))));
+            this.titleEditBox.Location = new System.Drawing.Point(3, 3);
+            this.titleEditBox.Name = "titleEditBox";
+            this.titleEditBox.Size = new System.Drawing.Size(101, 16);
+            this.titleEditBox.TabIndex = 2;
+            // 
+            // renameButton
+            // 
+            this.renameButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(89)))), ((int)(((byte)(89)))));
+            this.renameButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.renameButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.renameButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(182)))), ((int)(((byte)(182)))), ((int)(((byte)(182)))));
+            this.renameButton.Location = new System.Drawing.Point(110, 3);
+            this.renameButton.Name = "renameButton";
+            this.renameButton.Size = new System.Drawing.Size(42, 20);
+            this.renameButton.TabIndex = 3;
+            this.renameButton.Text = "Set";
+            this.renameButton.UseVisualStyleBackColor = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel1.Controls.Add(this.titleEditBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.renameButton, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(8, 8);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(155, 26);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // winListView
+            // 
+            this.winListView.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.winListView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.winListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.winListView.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.winListView.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(228)))), ((int)(((byte)(228)))));
+            this.winListView.Location = new System.Drawing.Point(8, 34);
+            this.winListView.Name = "winListView";
+            this.winListView.Size = new System.Drawing.Size(155, 360);
+            this.winListView.TabIndex = 5;
+            this.winListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            this.ClientSize = new System.Drawing.Size(171, 435);
+            this.Controls.Add(this.winListView);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.runButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "MainForm";
+            this.Padding = new System.Windows.Forms.Padding(8);
+            this.Text = "SJ Set Window Top Most";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TextBox titleEditBox;
+        private System.Windows.Forms.Button renameButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListView winListView;
+    }
+}
+
---
+++ win/SJSetWindowTopMost/SJSetWindowTopMost/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace SJSetWindowTopMost
+{
+
+    public partial class MainForm : Form
+    {
+        // public WindowInteropHelper(System.Windows.Window window);
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, int uFlags);
+
+    // ウィンドウをアクティブにする
+        public static void SetActiveWindow(IntPtr hWnd)
+        {
+            const int SWP_NOSIZE = 0x0001;
+            const int SWP_NOMOVE = 0x0002;
+            const int SWP_SHOWWINDOW = 0x0040;
+
+            // const int HWND_TOPMOST = -1;
+            // const int HWND_NOTOPMOST = -2;
+            const int HWND_TOPMOST = -1;
+            // const int HWND_NOTOPMOST = -2;
+
+            SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+            // SetWindowPos(hWnd, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_SHOWWINDOW | SWP_NOMOVE | SWP_NOSIZE);
+        }
+
+        public delegate bool EnumWindowsDelegate(IntPtr hWnd, IntPtr lparam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public extern static bool EnumWindows(EnumWindowsDelegate lpEnumFunc,
+            IntPtr lparam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowText(IntPtr hWnd,
+            StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetClassName(IntPtr hWnd,
+            StringBuilder lpClassName, int nMaxCount);
+
+        // [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        // private static extern int Set
+        // private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+        [DllImport("User32.Dll", EntryPoint = "SetWindowText")]
+        public static extern void SetWindowText(IntPtr hwnd, StringBuilder text);
+
+        private static bool EnumWindowCallBack(IntPtr hWnd, IntPtr lparam)
+        {
+            int cnt = 0;
+            //ウィンドウのタイトルの長さを取得する
+
+            int textLen = GetWindowTextLength(hWnd);
+            if (0 < textLen)
+            {
+                //ウィンドウのタイトルを取得する
+                StringBuilder tsb = new StringBuilder(textLen + 1);
+                GetWindowText(hWnd, tsb, tsb.Capacity);
+
+                //ウィンドウのクラス名を取得する
+                StringBuilder csb = new StringBuilder(256);
+                GetClassName(hWnd, csb, csb.Capacity);
+
+                if (tsb.ToString().IndexOf("Blender") != -1)
+                //if (tsb.ToString().IndexOf("32171920") != -1)
+                {
+                    StringBuilder newName = new StringBuilder("Blender" + cnt.ToString());
+                    //結果を表示する
+                    // MessageBox.Show("クラス名:" + csb.ToString());
+                    //MessageBox.Show("クラス名:" + tsb.ToString());
+                    // tsb
+
+                    SetWindowText(hWnd, newName);
+
+                    MessageBox.Show(hWnd.ToString());
+                    // SetActiveWindow(hWnd);
+                    cnt++;
+                    return true;
+                }
+
+                // Console.WriteLine("クラス名:" + csb.ToString());
+                // Console.WriteLine("タイトル:" + tsb.ToString());
+            }
+
+            //すべてのウィンドウを列挙する
+            return true;
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            //ウィンドウを列挙する
+            EnumWindows(new EnumWindowsDelegate(EnumWindowCallBack), IntPtr.Zero);
+
+            // MessageBox.Show(this.Handle.ToString());
+            // IntPtr handle = this.Handle;
+            // SetActiveWindow(handle);
+
+
+
+            foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses())
+            {
+                //メインウィンドウのタイトルがある時だけ列挙する
+                // MessageBox.Show(p.MainWindowTitle.Length.ToString());
+                if (p.MainWindowTitle.Length == 0)
+                {
+                    continue;
+
+                }
+                // MessageBox.Show(p.ProcessName + "  " + p.MainWindowTitle);
+                // MessageBox.Show(p.MainWindowTitle);
+                // MessageBox.Show(p.ProcessName);
+                // MessageBox.Show(p.MainWindowTitle);
+
+                //MessageBox.Show(p.ProcessName.IndexOf("blender").ToString());
+                //MessageBox.Show(p.ProcessName);
+
+                if (p.ProcessName.IndexOf("blender") != -1)
+                {
+                    //MessageBox.Show(p.MainWindowTitle);
+                    //p.MainWindowTitle = "Blender Main";
+                    //MessageBox.Show(p.ProcessName);
+                }
+            }
+            
+        }
+    }
+}
